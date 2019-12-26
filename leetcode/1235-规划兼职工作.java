a9d393da34aa6b555e4f676167d70991fb8886d1
2213483145@qq.com <2213483145@qq.com>
2019-12-26 15:57:12 +0800

    leetcode-dp

--- leetcode/1235-规划兼职工作.java
+++ leetcode/1235-规划兼职工作.java
@@ -2,28 +2,18 @@ class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         List<obj> list = new ArrayList<>();
         int n = startTime.length;
-        int i, j=n-1;
+        int i, j;
         for(i=0; i<n; ++i) {
             list.add(new obj(startTime[i], endTime[i], profit[i]));
         }
         Collections.sort(list);
-        for (i=0; i<n; ++i) {
-            obj x = list.get(i);
-            startTime[i] = x.st;
-            endTime[i] = x.et;
-            profit[i] = x.pt;
-            System.out.println(x.st+ " "+ x.et+" "+x.pt);
-        }
         int[] dp = new int[n+2];
         for(i=n-1; i>=0; --i) { //第一个到第n个
-            int st = startTime[i]; // 当前任务的开始时间
-            int et = endTime[i]; //当前任务结束时间
-            int pt = profit[i];// 当前任务产生的价值
-            j = Arrays.binarySearch(startTime, et);
+            obj x = list.get(i);
             for(j=i+1; j<n; ++j) {
-                if(startTime[j]>=et) break;
+                if(list.get(j).st>=x.et) break;
             }
-            dp[i] = Math.max(dp[i+1], pt+dp[j]);
+            dp[i] = Math.max(dp[i+1], x.pt+dp[j]);
         }
         return dp[0];
     }
@@ -36,7 +26,6 @@ class Solution {
             this.et = aet;
             this.pt = apt;
         }
-
         @Override
         public int compareTo(obj o) {
             return this.st-o.st;
