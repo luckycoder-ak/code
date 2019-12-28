819983ea73b713245027d6b672070604ab1d944e
2213483145@qq.com <2213483145@qq.com>
2019-12-28 15:54:58 +0800

    leetcode-dp condense

--- leetcode/63-不同路径II.java
+++ leetcode/63-不同路径II.java
@@ -3,14 +3,19 @@ class Solution {
         if(obstacleGrid == null) return 0;
         int n = obstacleGrid.length;//行数
         int m = obstacleGrid[0].length;//列数
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[2][m+1];
+        int k = 0;
         for(int i=1; i<=n; ++i) {
             for(int j=1; j<=m; ++j) {
-                if(obstacleGrid[i-1][j-1] == 1) continue;
-                if(i==1 && j==1) dp[i][j] = 1;
-                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                if(obstacleGrid[i-1][j-1] == 1) {
+                    dp[k][j] = 0;
+                    continue;
+                }
+                if(i==1 && j==1) dp[k][j] = 1;
+                else dp[k][j] = dp[k^1][j]+dp[k][j-1];
             }
+            k^=1;
         }
-        return dp[n][m];
+        return dp[(n%2)^1][m];
     }
 }
